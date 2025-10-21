3a88ccf541a4e46abacf91ed863f6373614d189c
KAVITHA <165481462+Kavithakarikolraja@users.noreply.github.com>
2025-10-21 20:43:45 +0530

    Time: 2 ms (68.01%) | Memory: 42.4 MB (44.55%) - LeetSync

---
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+
+        //Arrays.sort(arr1);
+        
+        int[] freq = new int[1001];
+        List<Integer> result = new ArrayList<>();
+
+        for(int n : arr1){
+            freq[n]++;
+        }
+
+        for(int n : arr2){
+           while(freq[n]>0){
+            result.add(n);
+            freq[n]--;
+           }
+            
+        }
+
+       for (int i = 0; i < freq.length; i++) {
+            while (freq[i] > 0) {
+                result.add(i);
+                freq[i]--;
+            }
+        }
+
+
+        return result.stream().mapToInt(Integer::intValue).toArray();
+
+    }
+}

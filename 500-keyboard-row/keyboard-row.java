24262654bc966de400ee8ecff8b9afb71ccef2b7
KAVITHA <165481462+Kavithakarikolraja@users.noreply.github.com>
2025-11-03 10:25:48 +0530

    Time: 1 ms (34.64%) | Memory: 42.9 MB (7.92%) - LeetSync

--- 500-keyboard-row/keyboard-row.java
+++ 500-keyboard-row/keyboard-row.java
@@ -1,27 +1,18 @@
 class Solution {
     public String[] findWords(String[] words) {
-        
-        Set<Character> row1 = new HashSet<>();
-        Set<Character> row2 = new HashSet<>();
-        Set<Character> row3 = new HashSet<>();
+        Map<Character, Integer> rowMap = new HashMap<>();
+        for (char ch : "qwertyuiopQWERTYUIOP".toCharArray()) rowMap.put(ch, 1);
+        for (char ch : "asdfghjklASDFGHJKL".toCharArray()) rowMap.put(ch, 2);
+        for (char ch : "zxcvbnmZXCVBNM".toCharArray()) rowMap.put(ch, 3);
 
-        for(char val : "qwertyuiopQWERTYUIOP".toCharArray()) row1.add(val);
-        for(char val : "asdfghjklASDFGHJKL".toCharArray()) row2.add(val);
-        for(char val : "zxcvbnmZXCVBNM".toCharArray()) row3.add(val);
-        
-        List<String> result = new ArrayList<>();      
+        List<String> result = new ArrayList<>();
 
         for (String word : words) {
-            Set<Character> targetRow = null;
-            char firstChar = word.charAt(0);
-
-            if (row1.contains(firstChar)) targetRow = row1;
-            else if (row2.contains(firstChar)) targetRow = row2;
-            else if (row3.contains(firstChar)) targetRow = row3;
-
+            int row = rowMap.get(word.charAt(0));
             boolean isValid = true;
-            for (char ch : word.toCharArray()) {
-                if (!targetRow.contains(ch)) {
+
+            for (int i = 1; i < word.length(); i++) {
+                if (rowMap.get(word.charAt(i)) != row) {
                     isValid = false;
                     break;
                 }
@@ -31,6 +22,5 @@ class Solution {
         }
 
         return result.toArray(new String[0]);
-  
     }
 }

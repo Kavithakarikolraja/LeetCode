fa9957613d994ec6b6ba55e766d11dad7417c1ac
KAVITHA <165481462+Kavithakarikolraja@users.noreply.github.com>
2025-11-07 21:40:17 +0530

    Time: 1 ms (35.65%) | Memory: 43.2 MB (5.78%) - LeetSync

---
+++ 824-number-of-lines-to-write-string/number-of-lines-to-write-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] numberOfLines(int[] widths, String s) {
+        int count = 0;
+        int line = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+            int w = widths[s.charAt(i) - 'a'];
+            if (count + w > 100) {
+                line++;
+                count = w;
+            } else {
+                count += w;
+            }
+        }
+
+        return new int[]{line, count};
+    }
+}

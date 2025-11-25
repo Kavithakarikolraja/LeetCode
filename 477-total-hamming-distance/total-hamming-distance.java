f4dd14f1c35e31088f0210ae7569319dc509dc56
KAVITHA <165481462+Kavithakarikolraja@users.noreply.github.com>
2025-11-25 21:59:55 +0530

    Time: 4 ms (90.20%) | Memory: 48 MB (23.28%) - LeetSync

--- 477-total-hamming-distance/total-hamming-distance.java
+++ 477-total-hamming-distance/total-hamming-distance.java
@@ -1,21 +1,16 @@
 class Solution {
     public int totalHammingDistance(int[] nums) {
-
-        Arrays.sort(nums);
-        int sum =0;
-        
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                
-                int val1 = nums[i];
-                int val2 = nums[j];
-
-                int val3 = val1^val2;
-                int count = Integer.bitCount(val3);
-                sum += count;
-            }
+        if (nums == null) {
+            return 0;
         }
-
-        return sum;
+        int distance = 0;
+        for (int i = 0; i < 32; i++) {
+            int one_count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                one_count += (nums[j] >> i) & 1;
+            }
+            distance += one_count * (nums.length - one_count);
+        }
+        return distance;
     }
 }

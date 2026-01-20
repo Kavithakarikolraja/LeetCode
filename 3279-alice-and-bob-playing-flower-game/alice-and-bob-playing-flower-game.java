c7b9cbd6a2773befc68a1aa9ef7e31f676c3517d
KAVITHA <165481462+Kavithakarikolraja@users.noreply.github.com>
2026-01-20 20:37:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (77.21%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.java
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.java
@@ -1,9 +1,9 @@
 class Solution {
     public long flowerGame(int n, int m) {
         
-       long oddx = (n+1)/2;
+       long oddx = (n+1L)/2;
        long evenx = n/2;
-       long oddy = (m+1)/2;
+       long oddy = (m+1L)/2;
        long eveny = m/2;
 
      return (oddx*eveny)+(oddy*evenx);

9776955f12cd8466f6ae042a0367b44a3e469881
KAVITHA <165481462+Kavithakarikolraja@users.noreply.github.com>
2026-01-21 20:56:34 +0530

    Time: 3 ms (71.29%) | Memory: 46.4 MB (50.12%) - LeetSync

--- 2312-most-frequent-number-following-key-in-an-array/most-frequent-number-following-key-in-an-array.java
+++ 2312-most-frequent-number-following-key-in-an-array/most-frequent-number-following-key-in-an-array.java
@@ -2,7 +2,7 @@ class Solution {
     public int mostFrequent(int[] nums, int key) {
 
         
-        int[] freq = new int[100001];
+       /* int[] freq = new int[100001];
 
         for(int i=0;i<nums.length-1;i++){
             if(nums[i]==key){
@@ -21,6 +21,29 @@ class Solution {
             }
         }
 
-        return res;
+        return res;*/
+
+        HashMap<Integer, Integer> freq = new HashMap<>();
+
+       
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == key) {
+                freq.put(nums[i + 1], freq.getOrDefault(nums[i + 1], 0) + 1);
+            }
+        }
+
+        int ans = 0;
+        int max = 0;
+
+      
+        for (int num : freq.keySet()) {
+            if (freq.get(num) > max) {
+                max = freq.get(num);
+                ans = num;
+            }
+        }
+
+        return ans;
+
     }
 }

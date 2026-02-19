9d6dfdb2bb37e0b316e0e4210956fca17a78975f
KAVITHA <165481462+Kavithakarikolraja@users.noreply.github.com>
2026-02-19 12:26:19 +0530

    Time: 4 ms (99.66%) | Memory: 47.2 MB (27.69%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,46 +1,78 @@
-class MinStack {
-
-    Node head;
-    Node head1;
-
-    class Node{
-            int data;
-            Node next;
-            Node(int val){
-              data = val;
-              next = null;
-            }
-        }
-
+/*class MinStack {
+    Stack<Integer> s;
     public MinStack() {
-        
-        
-        
+        s = new Stack<>();
     }
     
     public void push(int val) {
-
-        Node n1 = new Node(val);
-        Node n2 = new Node(val);
-        
-
-        if(head==null){
-            head = n1;
-        }else{
-            n1.next  =head;
-            head = n1;
+        s.push(val);
+    }
+    
+    public void pop() {
+        s.pop();
+    }
+    
+    public int top() {
+        return s.peek();
+    }
+    
+    public int getMin() {
+        int min = s.get(0);
+        for(int i = 1; i < s.size();i++){
+            min = Math.min(s.get(i),min);
         }
-        if(head1==null){
-            head1 = n2;
-        }else{
-            if(head1.data>=n2.data){
-                n2.next = head1;
-                head1 = n2;
+        return min;
+    }
+}/*
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+ class MinStack {
+    Node head,head1;
+    class Node{
+        int data;
+        Node next;
+        Node(int value){
+            data = value;
+            next = null;
+        }
+    }
+    public MinStack() {
+        head = null;
+        head1 = null;
+    }
+    
+    public void push(int val) {
+        Node newNode1 = new Node(val);
+        Node newNode2 = new Node(val);
+        if(head == null){
+            head = newNode1;
+        }
+        else{
+            newNode1.next = head;
+            head = newNode1;
+        }
+        if(head1 == null){
+            head1 = newNode2;
+        }
+        else{
+            if(head1.data >= newNode2.data){
+                newNode2.next = head1;
+                head1 = newNode2;
             }
         }
     }
     
     public void pop() {
+        if(head == null){
+            return;
+        }
         if(head.data == head1.data){
             head1 = head1.next;
         }
@@ -55,12 +87,4 @@ class MinStack {
         return head1.data;
     }
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
+ 

ef533fc007041d7e8837ed4725cf0b6e9204a722
KAVITHA <165481462+Kavithakarikolraja@users.noreply.github.com>
2026-03-04 22:08:44 +0530

    Time: 1 ms (81.05%) | Memory: 42.3 MB (46.42%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.java
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -3,11 +3,7 @@ class Solution {
         
         StringBuilder a = new StringBuilder(String.valueOf(num));
         
-        
-
-        int i;
-        
-        for(i=0;i<a.length();i++){
+        for(int i=0;i<a.length();i++){
             if(a.charAt(i)=='6'){
                 a.setCharAt(i,'9');
                 return Integer.parseInt(a.toString());

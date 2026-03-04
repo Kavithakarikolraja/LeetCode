151188ac004309f0c8f1152f7c82d575c3355e38
KAVITHA <165481462+Kavithakarikolraja@users.noreply.github.com>
2026-03-04 21:38:22 +0530

    Time: 2 ms (8.87%) | Memory: 45.1 MB (14.24%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        
+        boolean happen = false;
+        for(int i=nums.length-1;i>0;i--){
+            if(nums[i]<=nums[i-1]){
+                continue;
+            }else{
+                happen = true;
+                int pos = i-1;
+                int swap1 = nums[pos];
+                int swap2 = Integer.MAX_VALUE;
+                int pos1 =-1;
+                
+
+                for(int j=pos+1;j<nums.length;j++){
+                    if(swap1<nums[j]&&swap2>nums[j]){
+                        swap2 = nums[j];
+                        pos1 = j;
+                    }
+                }
+                if(pos1!=-1){
+                nums[pos] = swap2;
+                nums[pos1] = swap1;
+                Arrays.sort(nums, pos+1, nums.length);
+                }
+                  break;
+            }
+
+        }
+                if(happen==false){
+                    Arrays.sort(nums,0,nums.length);
+                }
+               
+            
+        
+    }
+}

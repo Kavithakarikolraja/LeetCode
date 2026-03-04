712fccf08b7a99bc26f5c7399bc713952d48d861
KAVITHA <165481462+Kavithakarikolraja@users.noreply.github.com>
2026-03-04 20:30:39 +0530

    Time: 0 ms (100.00%) | Memory: 47.9 MB (64.84%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,23 +1,21 @@
 class Solution {
     public double myPow(double x, int n) {
         
-        long  num = n;
-        if(num<0){
-            x = 1/x;
-            num = -num;
+        long num = n; 
+        if (num < 0) {
+            x = 1 / x;
+            num = -num; 
         }
-
-        double ans = 1;
-        double  current_value = x;
-
-        while(num>0){
-            if(num%2==1){
-                ans = ans*current_value;
-            }
-            current_value = current_value*current_value;
-            num /=2;
-        }
-
-    return ans;
+        return exp_by_squaring(x, num);
     }
-}
+
+    private double exp_by_squaring(double x, long n) {
+        if (n == 0) return 1;
+        if (n % 2 == 0) {
+            return exp_by_squaring(x * x, n / 2);
+        } else {
+            return x * exp_by_squaring(x * x, (n - 1) / 2);
+        }
+    }
+
+    }

ea7144780bd6bb4d12aa051bb3c867df4dd8f0de
KAVITHA <165481462+Kavithakarikolraja@users.noreply.github.com>
2026-03-04 19:59:11 +0530

    Time: 8 ms (89.90%) | Memory: 45.9 MB (85.44%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -5,7 +5,7 @@ class Solution {
         return false;
 
         while (n != 1) {
-            if (n % 3>0) {
+            if (n % 3!=0) {
                 return false; 
             }
             n = n / 3;

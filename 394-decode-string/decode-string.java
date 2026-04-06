33549163a102471e4b426810afefc9141ad8edca
KAVITHA <165481462+Kavithakarikolraja@users.noreply.github.com>
2026-04-06 20:36:40 +0530

    Time: 2 ms (34.43%) | Memory: 42.5 MB (96.99%) - LeetSync

---
+++ 394-decode-string/decode-string.java
@@ -0,0 +1,53 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<String> a = new Stack<>();
+        int pos = 0;
+
+        while(pos < s.length()) {
+            if(s.charAt(pos) == '[') {
+                a.push("[");
+                pos++;
+            } 
+
+            StringBuilder temp = new StringBuilder();
+            if(Character.isLetter(s.charAt(pos))) {
+                while(pos < s.length() && Character.isLetter(s.charAt(pos))) {
+                    temp.append(s.charAt(pos));
+                    pos++;
+                }
+                 if(temp.length()>0) {
+                    a.push(temp.toString());
+                }
+            }
+
+            StringBuilder temp_n = new StringBuilder();
+            if(pos < s.length() && Character.isDigit(s.charAt(pos))) {
+                while(pos < s.length() && Character.isDigit(s.charAt(pos))) {
+                    temp_n.append(s.charAt(pos));
+                    pos++;
+                }
+                if(temp_n.length()>0) {
+                    a.push(temp_n.toString());
+                }
+            }
+
+            if(pos < s.length() && s.charAt(pos) == ']') {
+                String res = "";
+                while(!a.peek().equals("[")) {
+                    res = a.pop() + res;
+                }
+                a.pop(); 
+                int repeat_count = Integer.parseInt(a.pop());
+                res = res.repeat(repeat_count);
+                a.push(res);
+                pos++;
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        while (!a.isEmpty()) {
+            result.insert(0, a.pop());
+        }
+        return result.toString(); 
+    }  
+}

aa9f28cfeab170ae566c239ef6ba0357330ebbfb
KAVITHA <165481462+Kavithakarikolraja@users.noreply.github.com>
2026-07-01 14:23:58 +0530

    Time: 12 ms (8.70%) | Memory: 46.6 MB (83.09%) - LeetSync

---
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int[] findMode(TreeNode root) {
+        
+        TreeMap<Integer,Integer> a = new TreeMap<>();
+
+        inorder(root,a);
+
+        ArrayList<Integer> res = new ArrayList<>();
+
+        int max = Integer.MIN_VALUE;
+
+        for(Map.Entry<Integer,Integer> e : a.entrySet())
+        {
+                max = Math.max(max,e.getValue());
+        }
+
+
+        for(Map.Entry<Integer,Integer> e : a.entrySet())
+        {
+            if(max == e.getValue())
+            {
+                res.add(e.getKey());
+            }
+                
+        }
+       
+       int[] arr = new int[res.size()];
+
+       for(int i=0;i<res.size();i++)
+       {
+        arr[i] = res.get(i);
+       }
+
+    return arr;
+
+    }
+
+    public void inorder(TreeNode root,TreeMap<Integer,Integer> a)
+    {
+        if(root==null) return;
+
+        inorder(root.left,a);
+        a.put(root.val,a.getOrDefault(root.val,0)+1);
+        inorder(root.right,a);
+    }
+}

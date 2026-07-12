3802d2f6d2eb5a3e1082fdd85e549f458a256269
KAVITHA <165481462+Kavithakarikolraja@users.noreply.github.com>
2026-07-12 21:00:16 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (67.55%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
@@ -1,7 +1,7 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
         
-        Set<Character> a = new HashSet<>();
+        /*Set<Character> a = new HashSet<>();
 
         for(Character w : word.toCharArray())
         {
@@ -16,6 +16,26 @@ class Solution {
             char s = (char)(c+32);
           
             if(word.indexOf(s)!=-1) count++;
+        }
+
+        return count;*/
+
+        boolean[] lower = new boolean[26];
+        boolean[] upper = new boolean[26];
+
+        for (char ch : word.toCharArray()) {
+            if (Character.isLowerCase(ch)) {
+                lower[ch - 'a'] = true;
+            } else {
+                upper[ch - 'A'] = true;
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < 26; i++) {
+            if (lower[i] && upper[i]) {
+                count++;
+            }
         }
 
         return count;

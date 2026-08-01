eb486a907c9c60419de6496c70398d56b2c75fa3
KAVITHA <165481462+Kavithakarikolraja@users.noreply.github.com>
2026-08-01 12:08:36 +0530

    Time: 3 ms (26.03%) | Memory: 47.4 MB (98.39%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
@@ -1,41 +1,29 @@
 class Solution {
   
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        
+
      List<List<Integer>> a = new ArrayList<>();
 
-     List<List<Integer>> edge = new ArrayList<>();
-     for(int i=0;i<graph.length;i++)
-     {
-        edge.add(new ArrayList<Integer>());
-     }
-
-     for(int i=0;i<graph.length;i++)
-     {
-        for(int j=0; j<graph[i].length;j++)
-        {
-            edge.get(i).add(graph[i][j]);
-        }
-     }
+    
 
      
    
-     dfs(edge,new ArrayList<Integer>(),a,0);
+     dfs(graph,new ArrayList<Integer>(),a,0);
 
      return a;   
     }
 
-    public static void dfs(List<List<Integer>> edge,List<Integer> temp,List<List<Integer>> result,int source)
+    public static void dfs(int[][] graph,List<Integer> temp,List<List<Integer>> result,int source)
     {
         temp.add(source);
-        if(source == edge.size()-1)
+        if(source == graph.length-1)
         {
             result.add(new ArrayList<>(temp));
         }
-        for(int j=0;j<edge.get(source).size();j++)
+        for(int j=0;j<graph[source].length;j++)
         {
-            //temp.add(edge.get(source).get(j));
-            dfs(edge,temp,result,edge.get(source).get(j));
+            
+            dfs(graph,temp,result,graph[source][j]);
         }
 
         temp.remove(temp.size()-1);

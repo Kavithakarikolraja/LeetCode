e2228f39545860aa0c6e60e04a9fcebf5d8bf732
KAVITHA <165481462+Kavithakarikolraja@users.noreply.github.com>
2026-08-11 11:16:33 +0530

    Time: 3 ms (69.81%) | Memory: 53.4 MB (91.11%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,53 +1,23 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        List<List<Integer>> a = new ArrayList<>();
+        int[] inDeg = new int[n+1];
+        int[] outDeg =  new int[n+1];
 
-        for(int i=0;i<=n;i++)
+        for(int ind = 0 ; ind < trust.length ; ind++)
         {
-            a.add(new ArrayList<Integer>());
+            int src = trust[ind][0];
+            int dest = trust[ind][1];
+            outDeg[src]++;
+            inDeg[dest]++;
         }
 
-        List<Integer> track = new ArrayList<>();
-        int sum = 0;
-    
-        for(int i=0;i<trust.length;i++)
+        for(int ind = 1 ; ind < n +1 ; ind++)
         {
-            a.get(trust[i][0]).add(trust[i][1]);
-            if(!track.contains(trust[i][0]))
+            if(outDeg[ind] == 0 && inDeg[ind] == n-1)
             {
-                sum += trust[i][0];
-                track.add(trust[i][0]);
+                return ind;
             }
         }
-
-        int tot = ((n*(n+1))/2);
-        int diff = tot-sum;
-        if(diff>n)
-        {
-             return -1;
-        }
-
-        
-       
-                boolean found = false;
-                for(int j=1;j<a.size();j++)
-                {
-                   if(diff==j) continue;
-                    if(!a.get(j).contains(diff))
-                    {
-                        found = true;
-                        break;
-                    }
-
-                }
-
-                if(!found)
-                {
-                    return diff;
-                }
-            
-        
-
         return -1;
     }
 }
